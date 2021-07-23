2fc956e70669db4cab2a77cfbda4acaf2733a2fe
rlsalcido24 <rlsalcido24>
2021-07-23 20:42:11 +0000

    appcode

---
+++ appcode.py
@@ -0,0 +1,15 @@
+# Databricks notebook source
+#insert app code
+os.environ["DATABRICKS_TOKEN"] = "<YOUR_TOKEN>"
+import os
+import requests
+import pandas as pd
+ 
+def score_model(dataset: pd.DataFrame):
+  url = 'https://<DATABRICKS_URL>/model/wine_quality/Production/invocations'
+  headers = {'Authorization': f'Bearer {os.environ.get("DATABRICKS_TOKEN")}'}
+  data_json = dataset.to_dict(orient='split')
+  response = requests.request(method='POST', headers=headers, url=url, json=data_json)
+  if response.status_code != 200:
+    raise Exception(f'Request failed with status {response.status_code}, {response.text}')
+  return response.json()
